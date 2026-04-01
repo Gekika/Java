9ef0e929310c5359c0b6baacf261f13e9ff0df5f
gekika <gkkaranja2@gmail.com>
2026-04-01 15:02:07 +0300

    Added another endpoint to help us lst the categories we have. We can now list categories.

--- backend/src/main/java/com/gekika/backend/controller/CategoryController.java
+++ backend/src/main/java/com/gekika/backend/controller/CategoryController.java
@@ -5,19 +5,23 @@ import com.gekika.backend.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Objects;
 
 @RestController
-@RequestMapping("v1/category")
+@RequestMapping("/v1/category")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Category>> listCategories(){
+        List<Category> body = categoryService.listcategories();
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
 
     @PostMapping("/")
     public ResponseEntity<Void> createCategory(@RequestBody Category category){
