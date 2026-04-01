f52a7f2b5d788d0afb71ad7302c286892ba77556
gekika <gkkaranja2@gmail.com>
2026-04-01 12:04:53 +0300

    Created the model layer, this is what will be created in the DataBase. I define the columns I want to store for Category.

---
+++ backend/src/main/java/com/gekika/backend/models/Category.java
@@ -0,0 +1,30 @@
+package com.gekika.backend.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private int id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+    private String description;
+    private String imageUrl;
+
+    public Category() { }
+
+    public Category(String categoryName, String description, String imageUrl) {
+        this.categoryName = categoryName;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+}
