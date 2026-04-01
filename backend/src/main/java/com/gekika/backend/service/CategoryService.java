66df8b1403028552404ea2c826d40b378a75079d
gekika <gkkaranja2@gmail.com>
2026-04-01 12:52:25 +0300

    Created the Service Layer for my application. This defines teh logic of where and how things move in the application. CRUD - you can:  1. Create a Category.  2. Read / List a Category. the whole list or by Id  3. Update a Category.  4. Delete a Category.

---
+++ backend/src/main/java/com/gekika/backend/service/CategoryService.java
@@ -0,0 +1,39 @@
+package com.gekika.backend.service;
+
+import com.gekika.backend.models.Category;
+import com.gekika.backend.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> listcategories(){
+        return categoryRepository.findAll();
+    }
+    public void createCategory(Category category) {
+        categoryRepository.save(category);
+    }
+    public Optional <Category> findCategoryById(Integer categoryId) {
+        return categoryRepository.findById(categoryId);
+    }
+    public Category findCategoryByCategoryName(String categoryName) {
+        return categoryRepository.findByCategoryName(categoryName);
+    }
+    public void deleteCategoryById(Integer categoryId) {
+        categoryRepository.deleteById(categoryId);
+    }
+    public void updateCategory(Integer categoryId, Category newcategory) {
+        Category category = categoryRepository.findById(categoryId).get();
+        category.setCategoryName(newcategory.getCategoryName());
+        category.setDescription(newcategory.getDescription());
+        category.setImageUrl(newcategory.getImageUrl());
+        categoryRepository.save(category);
+    }
+
+}
