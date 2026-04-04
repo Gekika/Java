660f7e7d46430a6df9642f57dcb59f20e3f1c3fd
gekika <gkkaranja2@gmail.com>
2026-04-04 12:48:13 +0300

    Refactored the code to make it more readable and clean

--- backend/src/main/java/com/gekika/backend/service/CategoryService.java
+++ backend/src/main/java/com/gekika/backend/service/CategoryService.java
@@ -14,21 +14,28 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
 
-    public List<Category> listcategories(){
+    public List<Category> listcategories() {
+
         return categoryRepository.findAll();
     }
+
     public void createCategory(Category category) {
         categoryRepository.save(category);
     }
-    public Optional <Category> readCategoryById(Integer categoryId) {
+
+    public Optional<Category> readCategoryById(Integer categoryId) {
+
         return categoryRepository.findById(categoryId);
     }
+
     public Category readCategoryByCategoryName(String categoryName) {
         return categoryRepository.findByCategoryName(categoryName);
     }
+
     public void deleteCategoryById(Integer categoryId) {
         categoryRepository.deleteById(categoryId);
     }
+
     public void updateCategory(Integer categoryId, Category newcategory) {
         Category category = categoryRepository.findById(categoryId).get();
         category.setCategoryName(newcategory.getCategoryName());
