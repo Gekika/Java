82e9036a507cab6ea1e239ec5aa6c7b8b24e2dee
gekika <gkkaranja2@gmail.com>
2026-04-04 14:03:44 +0300

    Created the repo class for products.

---
+++ backend/src/main/java/com/gekika/backend/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.gekika.backend.repository;
+
+import com.gekika.backend.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
